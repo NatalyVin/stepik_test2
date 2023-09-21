ad36e93882414a1f77fab3fc7a93120a399d9302
ns.vinokurova <vinokyroofa@gmail.com>
2023-09-22 01:29:57 +0300

    file

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import pytest
 import time
 from selenium.webdriver.common.by import By
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_button(browser):
     link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(7)
